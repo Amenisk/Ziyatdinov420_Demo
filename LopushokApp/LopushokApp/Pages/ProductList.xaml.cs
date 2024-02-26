7047c1a5f6dc5d7a368dc8e3c1d9a24a1fed924b
Amenisk <k.ziatdinov2003@gmail.com>
2024-02-26 17:32:35 +0300

    Added pagination

--- LopushokApp/LopushokApp/Pages/ProductList.xaml.cs
+++ LopushokApp/LopushokApp/Pages/ProductList.xaml.cs
@@ -21,11 +21,44 @@ namespace LopushokApp.Pages
     /// </summary>
     public partial class ProductList : Page
     {
+        int lastPage = 0;
         public ProductList()
         {
             InitializeComponent();
-            lvProducts.ItemsSource = App.Connection.Product.ToList();
-            Product prod = new Product();
+            List<string> names = new List<string>();
+            for(int i = 1; i < App.Connection.Product.ToList().Count() / 20; i++)
+            {
+                names.Add(i.ToString());
+                lastPage = i;
+            }
+
+            if(App.Connection.Product.ToList().Count() % 20 != 0)
+            {
+                lastPage++;
+                names.Add(lastPage.ToString());
+            }
+
+            lvPages.ItemsSource = names;
+            lvPages.SelectedIndex = 0;
+            lvProducts.ItemsSource = App.Connection.Product.ToList().Take(20);
+        }
+
+        private void MinusPage(object sender, RoutedEventArgs e)
+        {
+            if(lvPages.SelectedIndex != 0)
+            {
+                lvPages.SelectedIndex = --lvPages.SelectedIndex;
+                lvProducts.ItemsSource = App.Connection.Product.ToList().Skip(lvPages.SelectedIndex * 20).Take(20);
+            }
+        }
+
+        private void PlusPage(object sender, RoutedEventArgs e)
+        {
+            if (lvPages.SelectedIndex != lastPage - 1)
+            {
+                lvPages.SelectedIndex = ++lvPages.SelectedIndex;
+                lvProducts.ItemsSource = App.Connection.Product.ToList().Skip(lvPages.SelectedIndex * 20).Take(20);
+            }
         }
     }
 }
